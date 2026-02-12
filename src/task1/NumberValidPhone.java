9f35ccd11191c7d5f94adfe272703f431fc26250
DAVYDENKO OLEH <oleg.davudenko3817@gmail.com>
2026-02-13 01:20:45 +0200

    homework1

--- src/task1/NumberValidPhone.java
+++ src/task1/NumberValidPhone.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
-class printNumberValidPhone {
+class NumberValidPhone {
     static void main(String[] args) {
         printNumberValidPhone("src/task1/file.txt");
     }
